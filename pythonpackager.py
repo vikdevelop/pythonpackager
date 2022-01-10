1ad8c79b9af6188ab818c301006660273b9265c4
vikdevelop <83600218+vikdevelop@users.noreply.github.com>
2022-01-10 20:23:35 +0100

    Update pythonpackager.py

--- pythonpackager.py
+++ pythonpackager.py
@@ -2,6 +2,7 @@ import json
 import os
 import shutil
 
+
 print("PYTHONPACKAGER 1.0")
 with open("python-package.json") as jsonFile:
     jsonObject = json.load(jsonFile)
@@ -45,7 +46,7 @@ print("% s => installed" % icon)
 shutil.move('% s' % script, '% s/usr/local/app/scripts/' % name)
 print("% s => installed" % script)
 
-
+# Create pkginfo JSON file
 with open('pkg-details.json', 'w') as f:
     f.write('{\n')
     f.write('   "name": "% s",\n' % name)
@@ -54,22 +55,19 @@ with open('pkg-details.json', 'w') as f:
     f.write('   "script": "% s",\n' % script)
     f.write('   "desktop": "% s",\n' % desktop)
     f.write('   "icon": "% s",\n' % icon)
-    f.write('}\n')
-    f.write('{\n')
-    f.write('   "commands":\n')
-    f.write('       "1": "install -D % s.sh -t /usr/bin",\n' % name)
-    f.write('       "2": "install -D % s -t /usr/local/app/scripts",\n' % script)
-    f.write('       "3": "install -D % s -t /usr/share/applications",\n' % desktop)
-    f.write('       "4": "install -D % s -t /usr/share/icons/hicolor/128x128/apps",\n' % icon)
+    f.write('   "c1": "install -D usr/bin/% s.sh -t /usr/bin",\n' % name)
+    f.write('   "c2": "install -D usr/local/app/scripts/% s -t /usr/local/app/scripts",\n' % script)
+    f.write('   "c3": "install -D usr/share/applications/% s -t /usr/share/applications",\n' % desktop)
+    f.write('   "c4": "install -D usr/share/icons/hicolor/128x128/apps/% s -t /usr/share/icons/hicolor/128x128/apps"\n' % icon)
     f.write('}')
 pass
 
-shutil.move('pkg-details.json', '% s/' % name)
-print("Package info file (pkg-info.json) => created.")
+os.system('cp pkg-details.json ./% s/' % name)
+print("Package info file (pkg-details.json) => created.")
 
 # Create Package archive (compressed with zstd)
 os.system("tar --zstd -cf PACKAGE1.pythonpkg.tar.zst % s" % name)
-print("Archive (compressed with zstd) for % s package => created." % name)
+print("Archive (compressed with zstd) for % s package => compressed." % name)
 
-print("Python package was created successfully!")
+print("Python package was created SUCCESSFULLLY!")
 print("Your Python package file is named: % s.pypkg.tar.zst" % name)

4b2d3c55fb014887888be4a993c7383dbcd050b6
vikdevelop <83600218+vikdevelop@users.noreply.github.com>
2022-01-19 17:16:09 +0100

    Update installer.py

--- installer.py
+++ installer.py
@@ -1,13 +1,17 @@
 import os
 import json
 import shutil
+import linecache
 
 print("PYTHONPACKAGER 1.0")
 
-os.mkdir('pkgarchive-decompressed')
-os.system('tar xf PACKAGE1.pythonpkg.tar.zst -C ./pkgarchive-decompressed')
+os.chdir('package')
+"""
+# Uncomment this lines, if you need to extract archive of this package. You can also use the following command to extract package (type it into a Linux terminal (or console)):
+# tar xf name_version.pythonpkg.tar.zst -C ./
+os.system('tar xf % s_% s.pythonpkg.tar.zst -C ./' % (name, version))
 print("Archive for this pkg => decompressed.")
-
+"""
 with open("pkg-details.json") as jsonFile:
     jsonObject = json.load(jsonFile)
     jsonFile.close()
@@ -22,19 +26,23 @@ icon = jsonObject['icon']
 print("Reading data from pkg-details.json file...")
 print("Installing Package...")
 
-# Installation Package
-os.chdir('pkgarchive-decompressed/% s' % name)
+# Install Bash script
 shutil.move('usr/bin/% s.sh' % name, '/usr/bin/')
+# Install Desktop file
 shutil.move('usr/share/applications/% s' % desktop, '/usr/share/applications/')
+# Install App icon
 shutil.move('usr/share/icons/hicolor/128x128/apps/% s' % icon, '/usr/share/icons/hicolor/128x128/apps/')
-os.makedirs('/usr/local/app/scripts')
+# Create /usr/local/app/scripts directory
+if not os.path.exists('/usr/local/app/scripts'):
+    os.makedirs('/usr/local/app/scripts')
+pass
+# Install Python script(s)
 shutil.move('usr/local/app/scripts/% s' % script, '/usr/local/app/scripts/')
 
 print("Cleaning unnecessary files and directories...")
 
 os.system('cd')
-shutil.rmtree('pkgarchive-decompressed')
-os.remove('pkg-details.json')
-shutil.rmtree('% s' % name)
-
-print("Python Package: % s => installed!" % name)
+os.system('rm -rf ./pkgarchive-decompressed')
+os.system('rm ./pkg-details.json')
+os.system('rm -rf ./% s' % name)
+print("Python Package: % s => installed SUCCESSFULLY!" % name)

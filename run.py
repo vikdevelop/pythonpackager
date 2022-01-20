278059425ca8f103cfb798df888d7a11cc82f3a4
vikdevelop <83600218+vikdevelop@users.noreply.github.com>
2022-01-20 19:02:16 +0100

    Add files via upload

---
+++ run.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+import os
+
+answer = input("What do you want to do? ")
+if answer == "create":
+    os.system("python3 src/pythonpackager.py")
+elif answer == "install":
+    os.system("python3 src/installer.py")
+

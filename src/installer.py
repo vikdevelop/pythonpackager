cf947dcf2c878be448051c202cecb5f4598d7686
vikdevelop <83600218+vikdevelop@users.noreply.github.com>
2022-01-22 12:25:14 +0100

    Update installer.py

--- src/installer.py
+++ src/installer.py
@@ -9,8 +9,6 @@ os.chdir('package')
 """
 # Uncomment this lines, if you need to extract archive of this package. You can also use the following command to extract package (type it into a Linux terminal (or console)):
 # tar xf name_version.pythonpkg.tar.zst -C ./
-os.system('tar xf % s_% s.pythonpkg.tar.zst -C ./' % (name, version))
-print("Archive for this pkg => decompressed.")
 """
 with open("pkg-details.json") as jsonFile:
     jsonObject = json.load(jsonFile)
@@ -32,11 +30,14 @@ shutil.move('usr/bin/% s.sh' % name, '/usr/bin/')
 shutil.move('usr/share/applications/% s' % desktop, '/usr/share/applications/')
 # Install App icon
 shutil.move('usr/share/icons/hicolor/128x128/apps/% s' % icon, '/usr/share/icons/hicolor/128x128/apps/')
+
 # Create /usr/local/app/scripts directory
-if not os.path.exists('/usr/local/app/scripts'):
-    os.makedirs('/usr/local/app/scripts')
+if not os.path.exists('/usr/local/app/scripts/% s' % name):
+    os.makedirs('/usr/local/app/scripts/% s' % name)
 pass
+
 # Install Python script(s)
-shutil.move('usr/local/app/scripts/% s' % script, '/usr/local/app/scripts/')
+shutil.move('usr/local/app/scripts/% s' % script, '/usr/local/app/scripts/% s/' % name)
+shutil.move('pkg-details.json', '/usr/local/app/scripts/% s/' % name)
 
 print("Python Package: % s => installed SUCCESSFULLY!" % name)

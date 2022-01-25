68a2b2057160cc175e5ec74d116a7fcac2eb32cc
vikdevelop <83600218+vikdevelop@users.noreply.github.com>
2022-01-25 16:04:52 +0100

    Update run.py

--- run.py
+++ run.py
@@ -8,3 +8,9 @@ elif answer == "install":
     os.system("sudo python3 src/installer.py")
 elif answer == "uninstall":
     os.system("sudo python3 src/uninstall.py")
+elif answer == "help":
+    print("USAGE:\n")
+    print("options:\n")
+    print("- create: Create Python package from manifest file python-package.json\n")
+    print("- uninstall: Uninstall Python package from your Operationg system\n")
+    print("- install: Install your Python Package on your system.")

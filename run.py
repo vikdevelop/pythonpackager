6fa7966eb533b74f80f3e80aa3df0196a721f4e1
vikdevelop <83600218+vikdevelop@users.noreply.github.com>
2022-01-26 18:45:24 +0100

    Update run.py

--- run.py
+++ run.py
@@ -5,9 +5,9 @@ answer = input("What do you want to do? ")
 if answer == "create":
     os.system("python3 src/pythonpackager.py")
 elif answer == "install":
-    os.system("sudo python3 src/installer.py")
+    os.system("python3 src/installer.py")
 elif answer == "uninstall":
-    os.system("sudo python3 src/uninstall.py")
+    os.system("python3 src/uninstall.py")
 elif answer == "help":
     print("USAGE:\n")
     print("options:\n")

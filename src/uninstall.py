7a29c8240f6c3391f46e0ae10097e01afb9a0278
vikdevelop <83600218+vikdevelop@users.noreply.github.com>
2022-01-26 15:31:57 +0100

    Update uninstall.py

--- src/uninstall.py
+++ src/uninstall.py
@@ -4,7 +4,8 @@ import shutil
 
 pkgname = input('What package do you want to uninstall? ')
 
-with open("/usr/local/app/scripts/% s/pkg-details.json" % pkgname) as jsonFile:
+os.chdir(os.path.expanduser('~'))
+with open(".local/share/pythonpkgs/scripts/% s/pkg-details.json" % pkgname) as jsonFile:
     jsonObject = json.load(jsonFile)
     jsonFile.close()
 
@@ -15,8 +16,8 @@ summary = jsonObject['summary']
 script = jsonObject['script']
 desktop = jsonObject['desktop']
 
-shutil.rmtree('/usr/local/app/scripts/% s' % name)
-os.remove('/usr/share/applications/% s' % desktop)
-os.remove('/usr/bin/% s.sh' % name)
-os.remove('/usr/share/icons/hicolor/128x128/apps/% s' % icon)
+shutil.rmtree('.local/share/pythonpkgs/scripts/% s' % name)
+os.remove('.local/share/applications/% s' % desktop)
+os.remove('.local/bin/% s.sh' % name)
+os.remove('.local/share/icons/hicolor/128x128/apps/% s' % icon)
 print('Uninstalling package: % s...' % name, '\033[1m' + 'done.' + '\033[0m')

ff29b3fcff3c76299f901725026414e1c5f3714c
vikdevelop <83600218+vikdevelop@users.noreply.github.com>
2022-01-27 13:30:18 +0100

    Update run.py

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import os
 
-answer = input("What do you want to do? ")
+answer = input("What do you want to do (type 'help' for instructions of usage)? ")
 if answer == "create":
     os.system("python3 /app/src/pythonpackager.py")
 elif answer == "install":
@@ -9,8 +9,10 @@ elif answer == "install":
 elif answer == "uninstall":
     os.system("python3 /app/src/uninstall.py")
 elif answer == "help":
-    print("USAGE:\n")
-    print("options:\n")
-    print("- create: Create Python package from manifest file python-package.json\n")
-    print("- uninstall: Uninstall Python package from your Operationg system\n")
+    print("options:")
+    print("- create: Create Python package from manifest file python-package.json")
+    print("  - in the next step, you will need type path to package folder, want you create.")
+    print("- uninstall: Uninstall Python package from your Operating system")
+    print("  - in the next step, you will need type package name, want you uninstall.")
     print("- install: Install your Python Package on your system.")
+    print("  - in the next step, you will need type path to package, want you install.")

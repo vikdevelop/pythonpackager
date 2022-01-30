9c09b6401c2195140d5ee48157e8e88d431d224d
vikdevelop <83600218+vikdevelop@users.noreply.github.com>
2022-01-30 12:23:45 +0100

    Update run.py

--- run.py
+++ run.py
@@ -1,18 +1,23 @@
 #!/usr/bin/env python
 import os
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("-V", "--version", help="displays version of Python Packager and Python", action="store_true")
+parser.add_argument("-I", "--install", help="Install Python Package to your OS", action="store_true")
+parser.add_argument("-C", "--create", help="Create Python Package from manifest file python-package.json", action="store_true")
+parser.add_argument("-R", "--remove", help="Remove Python Package from your OS.", action="store_true")
 
-answer = input("What do you want to do (type 'help' for instructions of usage)? ")
-if answer == "create":
-    os.system("python3 /app/src/pythonpackager.py")
-elif answer == "install":
+args = parser.parse_args()
+
+if args.version:
+    print("Version of Python Packager: 1.1")
+    print(os.system("python3 --version"))
+
+if args.install:
     os.system("python3 /app/src/installer.py")
-elif answer == "uninstall":
+
+if args.create:
+    os.system("python3 /app/src/pythonpackager.py")
+
+if args.remove:
     os.system("python3 /app/src/uninstall.py")
-elif answer == "help":
-    print("options:")
-    print("- create: Create Python package from manifest file python-package.json")
-    print("  - in the next step, you will need type path to package folder, want you create.")
-    print("- uninstall: Uninstall Python package from your Operating system")
-    print("  - in the next step, you will need type package name, want you uninstall.")
-    print("- install: Install your Python Package on your system.")
-    print("  - in the next step, you will need type path to package, want you install.")

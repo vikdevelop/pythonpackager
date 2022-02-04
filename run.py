33e56ae4fb36d28bebae7fee6634d2e1bce1c3c4
vikdevelop <83600218+vikdevelop@users.noreply.github.com>
2022-02-04 22:16:19 +0100

    import argparse

--- run.py
+++ run.py
@@ -1,23 +1,37 @@
 #!/usr/bin/env python
 import os
 import argparse
+import sys
+import json
+def pkg_path(path):
+    if os.path.isdir(path):
+        return path
+    else:
+        raise argparse.ArgumentTypeError(f"dir {path} is not exists. Try entering a different path.")
+
 parser = argparse.ArgumentParser()
 parser.add_argument("-V", "--version", help="displays version of Python Packager and Python", action="store_true")
-parser.add_argument("-I", "--install", help="Install Python Package to your OS", action="store_true")
-parser.add_argument("-C", "--create", help="Create Python Package from manifest file python-package.json", action="store_true")
+parser.add_argument("-I", "--install", help="Install Python Package to your OS", type=pkg_path)
+parser.add_argument("-C", "--create", help="Create Python Package from manifest file python-package.json", type=pkg_path)
 parser.add_argument("-R", "--remove", help="Remove Python Package from your OS.", action="store_true")
 
 args = parser.parse_args()
 
 if args.version:
     print("Version of Python Packager: 1.1")
-    print(os.system("python3 --version"))
+    print("Version of Python:")
+    os.system('python3 --version')
 
 if args.install:
-    os.system("python3 /app/src/installer.py")
+    sys.path.append('/app/src')
+    os.chdir(args.install + '/package')
+    import installer
 
 if args.create:
-    os.system("python3 /app/src/pythonpackager.py")
+    sys.path.append('/app/src')
+    os.chdir(args.install + '/package')
+    import pythonpackager
 
 if args.remove:
-    os.system("python3 /app/src/uninstall.py")
+    sys.path.append('/app/src')
+    import uninstall

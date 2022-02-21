22530286dbf459b2ff8bda77fcf428645b03822f
vikdevelop <83600218+vikdevelop@users.noreply.github.com>
2022-02-21 18:29:27 +0100

    Update uninstall.py

--- src/uninstall.py
+++ src/uninstall.py
@@ -2,10 +2,7 @@ import os
 import json
 import shutil
 
-pkgname = input('What package do you want to uninstall? ')
-
-os.chdir(os.path.expanduser('~'))
-with open(".local/share/pythonpkgs/scripts/% s/pkg-details.json" % pkgname) as jsonFile:
+with open("pkg-details.json") as jsonFile:
     jsonObject = json.load(jsonFile)
     jsonFile.close()
 
@@ -16,8 +13,9 @@ summary = jsonObject['summary']
 script = jsonObject['script']
 desktop = jsonObject['desktop']
 
-shutil.rmtree('.local/share/pythonpkgs/scripts/% s' % name)
-os.remove('.local/share/applications/% s' % desktop)
-os.remove('.local/bin/% s.sh' % name)
-os.remove('.local/share/icons/hicolor/128x128/apps/% s' % icon)
+HOME = os.path.expanduser('~')
+shutil.rmtree('% s/.local/share/pythonpkgs/scripts/% s' % (HOME, name))
+os.remove('% s/.local/share/applications/% s' % (HOME, desktop))
+os.remove('% s/.local/bin/% s.sh' % (HOME, name))
+os.remove('% s/.local/share/icons/hicolor/128x128/apps/% s' % (HOME, icon))
 print('Uninstalling package: % s...' % name, '\033[1m' + 'done.' + '\033[0m')

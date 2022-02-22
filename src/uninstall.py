0f3a44d7e142aa61c23c37b7beb70d6b1b210178
vikdevelop <83600218+vikdevelop@users.noreply.github.com>
2022-02-22 11:35:23 +0100

    Update uninstall.py

--- src/uninstall.py
+++ src/uninstall.py
@@ -2,20 +2,26 @@ import os
 import json
 import shutil
 
-with open("pkg-details.json") as jsonFile:
-    jsonObject = json.load(jsonFile)
-    jsonFile.close()
+confirm = input("Are you sure? [Y/n]: ")
+if confirm == 'n':
+    print("Interrupted.")
+    exit()
 
-name = jsonObject['name']
-version = jsonObject['version']
-icon = jsonObject['icon']
-summary = jsonObject['summary']
-script = jsonObject['script']
-desktop = jsonObject['desktop']
+if confirm == 'Y' or 'y':
+    with open("pkg-details.json") as jsonFile:
+        jsonObject = json.load(jsonFile)
+        jsonFile.close()
 
-HOME = os.path.expanduser('~')
-shutil.rmtree('% s/.local/share/pythonpkgs/scripts/% s' % (HOME, name))
-os.remove('% s/.local/share/applications/% s' % (HOME, desktop))
-os.remove('% s/.local/bin/% s.sh' % (HOME, name))
-os.remove('% s/.local/share/icons/hicolor/128x128/apps/% s' % (HOME, icon))
-print('Uninstalling package: % s...' % name, '\033[1m' + 'done.' + '\033[0m')
+    name = jsonObject['name']
+    version = jsonObject['version']
+    icon = jsonObject['icon']
+    summary = jsonObject['summary']
+    script = jsonObject['script']
+    desktop = jsonObject['desktop']
+
+    HOME = os.path.expanduser('~')
+    shutil.rmtree('% s/.local/share/pythonpkgs/scripts/% s' % (HOME, name))
+    os.remove('% s/.local/share/applications/% s' % (HOME, desktop))
+    os.remove('% s/.local/bin/% s.sh' % (HOME, name))
+    os.remove('% s/.local/share/icons/hicolor/128x128/apps/% s' % (HOME, icon))
+    print('Uninstalling package: % s...' % name, '\033[1m' + 'done.' + '\033[0m')
